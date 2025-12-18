5bd74aab45b621b78769e7bc60c8543600358586
Lulu <bouange45@gmail.com>
2025-12-18 17:16:48 +0000

    dernier commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $produits = product::all();
+        return view('products.index', compact('produits'));
+    }
+
+    public function delete($id)
+    {
+        $produit = product::find($id);
+        if (!$produit) {
+            return redirect()->back()->with('error', 'Produit introuvable');
+        }
+        $produit->delete();
+        return redirect()->back()->with('success', 'Produit supprimé avec succès');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
+
+Route::get('/produits', [ProductController::class, 'index'])->name('products.index');
+    Route::delete('/produits/{id}', [ProductController::class, 'delete'])->name('products.delete');
