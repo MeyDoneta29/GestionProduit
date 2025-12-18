057ffb7165a995c9876f52c810a86c6461946a26
Lulu <bouange45@gmail.com>
2025-12-18 17:26:01 +0000

    desole les amis

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,19 +7,5 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        $produits = product::all();
-        return view('products.index', compact('produits'));
-    }
-
-    public function delete($id)
-    {
-        $produit = product::find($id);
-        if (!$produit) {
-            return redirect()->back()->with('error', 'Produit introuvable');
-        }
-        $produit->delete();
-        return redirect()->back()->with('success', 'Produit supprimé avec succès');
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,3 @@ Route::get('/', function () {
     return redirect()->route('products.index');
 });
 
-Route::get('/produits', [ProductController::class, 'index'])->name('products.index');
-    Route::delete('/produits/{id}', [ProductController::class, 'delete'])->name('products.delete');
