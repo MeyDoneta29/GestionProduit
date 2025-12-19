dd49181146b40c6432d1c4d71835fecd42bf8bfe
Lulu <bouange45@gmail.com>
2025-12-19 11:39:23 +0000

    fonction supprimer un produit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function delete($id)
+    {
+        $produit = Product::find($id);
+        if (!$produit) {
+            return redirect()->back()->with('error', 'Produit introuvable');
+        }
+        $produit->delete();
+        return redirect()->back()->with('success', 'Produit supprimé avec succès');
+    }
 }
