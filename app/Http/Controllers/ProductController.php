54696433dd940af7a4f26099f34b8a9a3434314e
Lulu <bouange45@gmail.com>
2025-12-19 12:16:34 +0000

    fonction modifier

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,14 +7,38 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        $produits = product::all();
+        return view('products.index', compact('produits'));
+    }
+
     public function create()
     {
         return view('products.create');
     }
 
-    /**
-     * Enregistrer un nouveau produit
-     */
+    public function edit($id)
+    {
+        $produit = product::findOrFail($id);
+        return view('products.edit', compact('produit'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0.01',
+            'quantity' => 'required|integer|min:0',
+        ]);
+
+        $produit = product::findOrFail($id);
+        $produit->update($validated);
+
+        return redirect()->route('products.index')->with('success', 'Produit mis à jour avec succès !');
+    }
+
     public function store(Request $request)
     {
         // Valider les données
@@ -27,7 +51,18 @@ class ProductController extends Controller
 
         // Créer le produit
         product::create($validated);
-
         return redirect()->route('products.create')->with('success', 'Produit ajouté avec succès !');
     }
+
+        public function delete($id)
+    {
+        $produit = product::find($id);
+        if (!$produit) {
+            return redirect()->back()->with('error', 'Produit introuvable');
+        }
+        $produit->delete();
+        return redirect()->back()->with('success', 'Produit supprimé avec succès');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-/*
+
 Route::get('/', function () {return redirect()->route('products.index');});
 
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-*/
+
 Route::get('products/create',[ProductController::class,'create'])->name('products.create');
 Route::post('products/store',[ProductController::class,'store'])->name('products.store');
+
+Route::get('products/{id}/edit',[ProductController::class,'edit'])->name('products.edit');
+Route::put('products/{id}',[ProductController::class,'update'])->name('products.update');
+
+Route::delete('/products/{id}', [ProductController::class, 'delete'])->name('products.delete');
